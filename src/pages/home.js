028224956e6f41976ff429a22f1ab3feb9cfb9c9
Vlados2741 <ramosinho3@gmail.com>
2022-11-11 06:44:55 +0200

    2

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,27 @@
 // import { useState } from 'react';
+import axios from 'axios';
+import { NavLink } from 'react-router-dom';
 
-const request =
-  'https://api.themoviedb.org/3/trending/all/day?api_key=2994e3a31c3cad99fd99bf3fe61d916f';
+// const request =
+//   'https://api.themoviedb.org/3/trending/all/day?api_key=2994e3a31c3cad99fd99bf3fe61d916f';
 
-export const Home = () => {
+export const Home = async () => {
   // const [trendList, useTrendList] = useState('');
 
-  const fetchMovies = async () => {
-    const response = await fetch(request);
-    // useTrendList(response);
-    console.log(response);
-  };
-  fetchMovies();
-  return <h1>Trending today</h1>;
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=2994e3a31c3cad99fd99bf3fe61d916f&language=en-US`
+  );
+  // useTrendList(response);
+  const result = response.data.results;
+
+  return (
+    <div>
+      <h1>Trending today</h1>
+      <ul>
+        {result.map(({ title }) => (
+          <NavLink to="/film">{title}</NavLink>
+        ))}
+      </ul>
+    </div>
+  );
 };
