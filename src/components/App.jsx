e3fb81a01fee0d6ba018f0908585d39ace5691f4
Vlados2741 <ramosinho3@gmail.com>
2022-11-29 11:27:40 +0200

    fix2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import './styles.css';
 
-export const App = () => {
-  const Home = lazy(() => import('../pages/home'));
-  const Movie = lazy(() => import('../pages/movies'));
-  const MoviePage = lazy(() => import('../pages/movie-page'));
-  const Cast = lazy(() => import('../pages/cast'));
-  const Reviews = lazy(() => import('../pages/reviews'));
-  const NotFound = lazy(() => import('../pages/not-found'));
+const Home = lazy(() => import('../pages/home'));
+const Movie = lazy(() => import('../pages/movies'));
+const MoviePage = lazy(() => import('../pages/movie-page'));
+const Cast = lazy(() => import('../pages/cast'));
+const Reviews = lazy(() => import('../pages/reviews'));
+const NotFound = lazy(() => import('../pages/not-found'));
 
+export const App = () => {
   return (
     <Suspense fallback={<div></div>}>
       <Routes>
